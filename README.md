51f3d84139497ae3e587bdc2547ff0e45931af93
simaki <shota.imaki@icloud.com>
2020-09-19 13:24:25 +0900

    init

---
+++ README.md
@@ -0,0 +1,78 @@
+# PyTorch Activations
+
+## ELU activation function
+
+![ELU activation function](fig/ELU.png)
+
+## Hardshrink activation function
+
+![Hardshrink activation function](fig/Hardshrink.png)
+
+## Hardsigmoid activation function
+
+![Hardsigmoid activation function](fig/Hardsigmoid.png)
+
+## Hardtanh activation function
+
+![Hardtanh activation function](fig/Hardtanh.png)
+
+## Hardswish activation function
+
+![Hardswish activation function](fig/Hardswish.png)
+
+## LeakyReLU activation function
+
+![LeakyReLU activation function](fig/LeakyReLU.png)
+
+## LogSigmoid activation function
+
+![LogSigmoid activation function](fig/LogSigmoid.png)
+
+## ReLU activation function
+
+![ReLU activation function](fig/ReLU.png)
+
+## ReLU6 activation function
+
+![ReLU6 activation function](fig/ReLU6.png)
+
+## SELU activation function
+
+![SELU activation function](fig/SELU.png)
+
+## CELU activation function
+
+![CELU activation function](fig/CELU.png)
+
+## GELU activation function
+
+![GELU activation function](fig/GELU.png)
+
+## Sigmoid activation function
+
+![Sigmoid activation function](fig/Sigmoid.png)
+
+## Softplus activation function
+
+![Softplus activation function](fig/Softplus.png)
+
+## Softshrink activation function
+
+![Softshrink activation function](fig/Softshrink.png)
+
+## Softsign activation function
+
+![Softsign activation function](fig/Softsign.png)
+
+## Tanh activation function
+
+![Tanh activation function](fig/Tanh.png)
+
+## Tanhshrink activation function
+
+![Tanhshrink activation function](fig/Tanhshrink.png)
+
+## Threshold activation function
+
+![Threshold activation function](fig/Threshold.png)
+
---
+++ make.py
@@ -0,0 +1,64 @@
+import pathlib
+
+import torch
+import matplotlib.pyplot as plt
+import seaborn
+
+
+def get_activations():
+    yield torch.nn.ELU()
+    yield torch.nn.Hardshrink()
+    yield torch.nn.Hardsigmoid()
+    yield torch.nn.Hardtanh()
+    yield torch.nn.Hardswish()
+    yield torch.nn.LeakyReLU()
+    yield torch.nn.LogSigmoid()
+    # yield torch.nn.MultiheadAttention()
+    # yield torch.nn.PReLU()
+    yield torch.nn.ReLU()
+    yield torch.nn.ReLU6()
+    yield torch.nn.SELU()
+    yield torch.nn.CELU()
+    yield torch.nn.GELU()
+    yield torch.nn.Sigmoid()
+    yield torch.nn.Softplus()
+    yield torch.nn.Softshrink()
+    yield torch.nn.Softsign()
+    yield torch.nn.Tanh()
+    yield torch.nn.Tanhshrink()
+    yield torch.nn.Threshold(threshold=1, value=1)
+
+
+def draw(activation):
+    name = activation.__class__.__name__
+    x = torch.linspace(-6, 6, 100)
+    y = activation(x)
+
+    plt.figure()
+    plt.plot(x, y)
+    plt.xlabel("Input")
+    plt.ylabel("Output")
+    plt.xlim(-6, 6)
+    plt.ylim(-6, 6)
+    plt.title("{} activation function".format(name))
+    plt.savefig("fig/{}.png".format(name))
+    plt.close()
+
+
+def write(activation, f):
+    name = activation.__class__.__name__
+    f.write("## {} activation function\n\n".format(name))
+    f.write("![{} activation function](fig/{}.png)\n\n".format(name, name))
+
+
+def main():
+    seaborn.set_style("whitegrid")
+    with open("README.md", "w") as f:
+        f.write("# PyTorch Activations\n\n")
+        for activation in get_activations():
+            draw(activation)
+            write(activation, f)
+
+
+if __name__ == "__main__":
+    main()
